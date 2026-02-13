740f1a4be2b96948e9d4c8c751711123554c75e3
Akash Das <akashdas@Akashs-MacBook-Air-2.local>
2026-02-13 13:03:16 +0530

    final changes done

--- src/auth/salesforceConfig.ts
+++
@@ -1,8 +0,0 @@
-export const SALESFORCE_DOMAIN =
-  "https://2creativercaplayground-dev-ed.develop.my.salesforce.com";
-
-export const CLIENT_ID =
-  "3MVG9ecDCcZXF8nSpuXZSzLrC5dcoJrjFtcp48C8hFrHGexGLJAxKu0UnGZPofjQT8Z_sz7NNy5P6T70f8F.z";
-
-export const REDIRECT_URI =
-  "http://localhost:5173/oauth/callback";
